dbc1fdfd9bc09a14586d3d2cfc5fa65af6d3a277
KimberlyYen <87320011+KimberlyYen@users.noreply.github.com>
2021-11-16 17:45:56 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="container flex flex-col mx-auto items-center">
+      <h2 class="text-gray-500 m-3 mt-10">Hanns Care Analyze</h2>
+      <p class="text-gray-500 text-xs mb-5" >User using HannsCare APP function click times record</p>
+        <div style="max-width: 400px">
+        <vue3-chart-js v-bind="{ ...pieChart }" />
+    </div>
+  </div>
+</template>
+
+<script>
+import Vue3ChartJs from "@j-t-mcc/vue3-chartjs";
+
+export default {
+  name: "App",
+  components: {
+    Vue3ChartJs,
+  },
+  setup() {
+    const pieChart = {
+      type: "pie",
+      data: {
+        labels: ["Step", "Distance", "Sleep"],
+        datasets: [
+          {
+            backgroundColor: [
+              'rgb(75, 192, 192)',
+              'rgb(255, 205, 86)',
+              'rgb(54, 162, 235)',
+              'rgb(255, 99, 132)',
+            ],
+            data: [80, 20, 40],
+          },
+        ],
+      },
+    };
+
+    return {
+      pieChart,
+    };
+  },
+};
+</script>
